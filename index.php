dd34a53c37c36529a009d5b68bbc9c950e0aa12b
Md Hg Tushar <81422400+mdhgtushar@users.noreply.github.com>
2021-06-28 21:11:01 +0600

    Initial commit

---
+++ index.php
@@ -0,0 +1,55 @@
+<?php include"inc/header.php";?>
+<div class="container">
+<div class="row">
+<div class="col-lg-8">
+
+
+<a href="page.php" class="card mb-3 text-decoration-none text-muted">
+<div class="row g-0">
+<div class="col-md-4">
+<img  style="width: 100%;height: 150px;object-fit: cover;" src="https://codingyaar.com/wp-content/uploads/bootstrap-4-card-image-left-demo-image.jpg" class="card-img-top" />
+</div>
+<div class="col-md-8">
+<div class="card-body">
+<h5 class="card-title">সহজেই খুজে পান ট্রিকবিডি এর সাপোর্ট টিম কে… যেকোন প্রয়োজনে আপনার পাশে :)</h5>
+<span class="d-inline-block text-truncate col-12" style="font-size: 12px;">
+আসসলামুআলাইকুম, ট্রিকবিডি এর গুরুত্বপূর্ন কিছু আপডেট নিয়ে আপনাদের সামনে হাজির হলাম। 
+অনেকেরেই অভিযোগ ছিলো ট্রিকবিডি টিম এর সাথে যোগাযোগ এর..
+</span>
+<div class="badge bg-primary text-wrap">
+Lifestyle
+</div>
+<small class="text-muted">3 mins ago</small>
+</div>
+</div>
+</div>
+</a>
+
+
+
+
+
+<nav aria-label="Page navigation example">
+<ul class="pagination">
+<li class="page-item">
+<a class="page-link" href="#" aria-label="Previous">
+<span aria-hidden="true">&laquo;</span>
+</a>
+</li>
+<li class="page-item"><a class="page-link" href="#">1</a></li>
+<li class="page-item"><a class="page-link" href="#">2</a></li>
+<li class="page-item"><a class="page-link" href="#">3</a></li>
+<li class="page-item">
+<a class="page-link" href="#" aria-label="Next">
+<span aria-hidden="true">&raquo;</span>
+</a>
+</li>
+</ul>
+</nav>
+</div>
+<div class="col-lg-4">
+<?php include"inc/sidebar.php";?>
+</div>
+
+</div>
+<?php include"inc/footer.php";?>
